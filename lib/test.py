bd0e2e69ffe6609b0f4ca8d0ef00caacf30b3b6b
Lijiawen <nerver0stop@163.com>
2019-02-16 15:12:36 +0900

    test module

---
+++ lib/test.py
@@ -0,0 +1,13 @@
+import random
+
+def test_print():
+	print("hello")
+
+def test_get_array():
+	A = []
+	for i in range(100):
+		A.append(int(random.random()*100))
+	return A
+
+def __init__():
+	pass
